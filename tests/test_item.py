de96796708b5f2dbdc12c347f02fc8c3eed9a65c
Maxim Shurygin <maximys2142@mail.ru>
2023-06-13 17:08:29 +0700

    added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Test", 10, 5)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 50
