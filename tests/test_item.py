09d2e5e065841d81d1a433dbde67d9d75a1c13b4
Maxim Shurygin <maximys2142@mail.ru>
2023-06-16 22:28:45 +0700

    added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -16,3 +16,14 @@ def test_apply_discount(item):
     assert item.price == 10
     item.apply_discount()
     assert item.price == 10 * Item.pay_rate
+
+
+def test_string_to_number(item):
+    assert item.string_to_number('7.7') == 7
+
+
+def test_instantiate_from_csv(item):
+    item.instantiate_from_csv()
+    assert len(item.all)
+
+
