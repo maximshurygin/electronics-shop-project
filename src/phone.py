c5c9f906ada0e84c9229c56e2d17708ef936a84a
Maxim Shurygin <maximys2142@mail.ru>
2023-06-27 22:24:17 +0400

    added __repr__

--- src/phone.py
+++ src/phone.py
@@ -6,6 +6,10 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
+    def __repr__(self):
+        """возвращает описание для разработчика"""
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
     @property
     def number_of_sim(self):
         return self.__number_of_sim
