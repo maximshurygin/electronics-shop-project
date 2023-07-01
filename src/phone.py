8c3d85ca0190e68bd8e1f13e8808f59eabbb00be
Maxim Shurygin <maximys2142@mail.ru>
2023-07-01 10:02:45 +0400

    small fix

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,7 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
+        self.number_of_sim = number_of_sim
 
     def __repr__(self):
         """возвращает описание для разработчика"""
